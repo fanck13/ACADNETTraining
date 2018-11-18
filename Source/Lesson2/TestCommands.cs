fdbdc43e2ce511c71d8980037981f1d7c6202fe8
Summer Ding <zqding@hotmail.com>
2018-11-18 11:10:34 +0800

    Update lesson2

--- Source/Lesson2/TestCommands.cs
+++ Source/Lesson2/TestCommands.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System;
 using Autodesk.AutoCAD.Geometry;
 using System.Drawing;
+using System.Linq;
+using AcApp = Autodesk.AutoCAD.ApplicationServices.Application;
 
 [assembly: CommandClass(typeof(ACADPlugin.TestCmd))]
 
@@ -14,26 +16,48 @@ namespace ACADPlugin
 {
     public class TestCmd
     {
-        [CommandMethod("ModifyCircle")]
-        public void cmdModifyCircle()
+        [CommandMethod("ListModelSpaceInfo")]
+        public void cmdListModelSpaceInfo()
         {
-            var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            var doc = AcApp.DocumentManager.MdiActiveDocument;
             var ed = doc.Editor;
-            var db = doc.Database;
-
-            var result = ed.GetEntity("\nPlease select a Circle\n");
-            if (result.Status != PromptStatus.OK) return;
+            var db = doc.Database; 
 
             using (var tr = db.TransactionManager.StartTransaction())
             {
-                var circle = tr.GetObject(result.ObjectId, OpenMode.ForWrite) as Circle;
+                var bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                var btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForRead) as BlockTableRecord;
 
-                circle.Center = new Point3d(circle.Center.X + 100, circle.Center.Y + 50, circle.Center.Z);
-                circle.Radius = circle.Radius * 2;
-                circle.Color = Autodesk.AutoCAD.Colors.Color.FromRgb(255, 0, 0);    //Red
+                foreach (ObjectId id in btr)
+                {
+                    var entity = tr.GetObject(id, OpenMode.ForRead) as Entity;
+                    ed.WriteMessage(string.Format("\nEntity type : {0}", entity.GetRXClass().Name));
+                }
 
-                tr.Commit();
+                //LINQ
+                //btr.Cast<ObjectId>().ToList().ForEach(i => ed.WriteMessage(string.Format("\nEntity type : {0}", (tr.GetObject(i, OpenMode.ForRead) as Entity).GetRXClass().Name)));
             }
+        }
+
+        [CommandMethod("ListLayoutDictInfo")]
+        public void cmdListLayoutDictInfo()
+        {
+            var doc = AcApp.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
+            var db = doc.Database;
+
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                var layoutDict = tr.GetObject(db.LayoutDictionaryId, OpenMode.ForRead) as DBDictionary;
+                foreach (var layoutEntry in layoutDict)
+                {
+                    var layout = tr.GetObject(layoutEntry.Value, OpenMode.ForRead) as Layout;
+                    ed.WriteMessage(string.Format("\nLayout name : {0}", layout.LayoutName));
+                }
+
+                //LINQ
+                //layoutDict.Cast<DBDictionaryEntry>().ToList().ForEach(i => ed.WriteMessage(string.Format("\nLayout name : {0}", (tr.GetObject(i.Value, OpenMode.ForRead)).GetRXClass().Name)));
+            } 
         }
     }
 }
