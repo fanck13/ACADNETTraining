0046fde10fcfd83a90d01ad264c90509e6f15d0b
Summer Ding <zqding@hotmail.com>
2018-11-18 21:53:55 +0800

    Add lesson8

---
+++ Source/Lesson8/TestCommands.cs
@@ -0,0 +1,93 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Linq;
+using AcApp = Autodesk.AutoCAD.ApplicationServices.Application;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+[assembly: CommandClass(typeof(ACADPlugin.TestCmd))]
+
+namespace ACADPlugin
+{
+    public class TestCmd
+    {
+        [CommandMethod("CreateBox")]
+        public void cmdCreateBox()
+        {
+            var doc = AcApp.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
+            var db = doc.Database;
+
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                var bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                var btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                var box = new Solid3d();
+                box.CreateBox(100, 200, 300);
+
+                var matrix = ed.CurrentUserCoordinateSystem;
+                matrix = matrix * Matrix3d.Displacement(new Vector3d(111, 222, 333));
+                box.TransformBy(matrix);
+
+                btr.AppendEntity(box);
+                tr.AddNewlyCreatedDBObject(box, true);
+
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("HighlightRegion")]
+        public void cmdHighlightRegion()
+        {
+            var doc = AcApp.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
+            var db = doc.Database;
+
+            var pso = new PromptSelectionOptions();
+            pso.SingleOnly = true;
+            pso.ForceSubSelections = true;
+            var psr = ed.GetSelection(pso);
+            if (psr.Status != PromptStatus.OK) return;
+
+            var sSet = psr.Value;
+            var sObj = sSet[0];
+            var sSubObjs = sObj.GetSubentities();
+            var regionSubId = sSubObjs[0].FullSubentityPath.SubentId;
+            if (regionSubId.Type != SubentityType.Face) return;
+
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                var solid3d = tr.GetObject(sObj.ObjectId, OpenMode.ForWrite) as Solid3d;
+                FullSubentityPath regionPath = new FullSubentityPath(new ObjectId[] { sObj.ObjectId }, regionSubId);
+                solid3d.Highlight(regionPath, false);
+                solid3d.SetSubentityColor(regionSubId, Autodesk.AutoCAD.Colors.Color.FromRgb(255, 0, 0));
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("BooleanBoxes")]
+        public void cmdBooleanBoxes()
+        {
+            var doc = AcApp.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
+            var db = doc.Database;
+
+            var result1 = ed.GetEntity("\nPlease select first Box");
+            if (result1.Status != PromptStatus.OK) return;
+            var result2 = ed.GetEntity("\nPlease select second Box");
+            if (result2.Status != PromptStatus.OK) return;
+
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                var box1 = tr.GetObject(result1.ObjectId, OpenMode.ForWrite) as Solid3d;
+                var box2 = tr.GetObject(result2.ObjectId, OpenMode.ForWrite) as Solid3d;
+                box1.BooleanOperation(BooleanOperationType.BoolUnite, box2);
+
+                tr.Commit();
+            }
+        }
+    }
+}
+
